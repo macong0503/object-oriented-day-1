36c1b8f5fc69dbf7c5724e620d8a7ae77bcaa3f5
macong0503 <1512346520@qq.com>
2021-07-26 20:21:18 +0800

    Create GenericMethodTest.java

---
+++ Text/Generic/GenericMethodTest.java
@@ -0,0 +1,10 @@
+package Text.Generic;
+
+public class GenericMethodTest {
+    public static void main(String[] args) {
+        GenericMethod g = new GenericMethod();
+        g.methodA("董公子");
+        g.methodA(112);
+        g.methodA(true);
+    }
+}
